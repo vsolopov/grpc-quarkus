99f47da3d016ce8364bab8f69d232de54cb8a7f2
vsolopov <vladyslav.solopov@gmail.com>
2024-03-12 16:37:33 +0200

    add client-server

---
+++ src/main/java/com/solopov/grpc/HelloService.java
@@ -0,0 +1,18 @@
+package com.solopov.grpc;
+
+import io.quarkus.example.Greeter;
+import io.quarkus.example.HelloReply;
+import io.quarkus.example.HelloRequest;
+import io.quarkus.grpc.GrpcService;
+import io.smallrye.mutiny.Uni;
+
+@GrpcService
+public class HelloService implements Greeter {
+
+    @Override
+    public Uni<HelloReply> sayHello(HelloRequest request) {
+        return Uni.createFrom().item(() ->
+                HelloReply.newBuilder().setMessage("Hello " + request.getName()).build()
+        );
+    }
+}
